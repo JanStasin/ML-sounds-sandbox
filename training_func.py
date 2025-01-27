35303becd08bad92338a5a4eff7c2656e59f4a5f
Stasinski, Jan <jan.stasinski@bih-charite.de>
2025-01-27 23:12:04 +0100

    scripts for exploration and new folder structure, working models are now stored in results file in working_models directory

---
+++ training_func.py
@@ -0,0 +1,72 @@
+import torch
+import torchaudio
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+import random
+
+import seaborn as sns
+
+import numpy as np
+
+def run_training(model, train_loader, val_loader, n_classes, rate_l=0.001, NUM_EPOCHS=800, save=True):
+    # %% TRAINING
+    #model = AudioClassifNet()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=rate_l)
+    losses_epoch_mean = []
+    for epoch in range(NUM_EPOCHS):
+        losses_epoch = []
+        for i, data in enumerate(train_loader):
+            inputs, labels = data
+            # Check for NaN loss
+            if torch.isnan(inputs).any():
+                print(f"NaN input at epoch {epoch}, batch {i}")
+                i_err = inputs
+                break
+            
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+            
+            # Check for NaN loss
+            if torch.isnan(loss):
+                print(f"NaN loss at epoch {epoch}, batch {i}")
+                l_err = loss
+                break
+            
+            loss.backward()
+            optimizer.step()
+            losses_epoch.append(loss.item())
+        
+        losses_epoch_mean.append(np.mean(losses_epoch))
+        if epoch % (int(NUM_EPOCHS/10)) == 0:
+            print(f'Epoch {epoch}/{NUM_EPOCHS}, Loss: {np.mean(losses_epoch):.16f}')
+
+    sns.lineplot(x=list(range(len(losses_epoch_mean))), y=losses_epoch_mean)
+    y_val = []
+    y_val_hat = []
+    for i, data in enumerate(val_loader):
+        inputs, y_val_temp = data
+        with torch.no_grad():
+            y_val_hat_temp = model(inputs).round()
+    
+        y_val.extend(y_val_temp.numpy())
+        y_val_hat.extend(y_val_hat_temp.numpy())
+    
+    # Accuracy
+    acc = accuracy_score(y_val, np.argmax(y_val_hat, axis=1))
+
+    print(f'Accuracy lr={rate_l}: {acc*100:.2f} %')
+    # confusion matrix
+    cm = confusion_matrix(y_val, np.argmax(y_val_hat, axis=1))
+    if save and acc*100> 60:
+        print('saving')
+        m = torch.save(model.state_dict(), f'audio_classification_model10_LR{rate_l}_a{acc*100:.0f}%.pth')
+        data = {'mean_loss': losses_epoch_mean, 'acc':acc, 'cm': cm, 'model': m }
+        np.save(f'outputs/results_and_model_acc_{acc*100:.1f}_LR_{rate_l}_nclasses_{n_classes}',data) 
+
+    return losses_epoch_mean, acc, cm
---
+++ training_script_exp.py
@@ -0,0 +1,68 @@
+import os
+import librosa
+import librosa.display
+import numpy as np
+# plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+
+import torch
+import torchaudio
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+import random
+
+## import custom made datset class:
+from audio_ds_model import AudioDataset, AudioClassifNet
+## and the external trainig function:
+from training_func import run_training
+
+# load preprocessed spectrograms data:
+dict_mats = np.load('/Users/jansta/learn/acoustics/dict_mats.npy', allow_pickle=True).item()
+all_labels = list(dict_mats['A'].keys())
+
+transform = transforms.Compose(
+    [transforms.Resize((64,431)),
+    transforms.Grayscale(num_output_channels=1),
+    #transforms.ToTensor(),
+    transforms.Normalize((0.5, ), (0.5, ))])
+
+
+NUM_EPOCHS = 700
+LR = 0.001
+
+#choose_labels:
+for n in range(4,25,4):
+    chosen_labels = all_labels[:n]
+    print(f'Number of labels: {n} --> {chosen_labels}')
+    print(f'Epochs  {NUM_EPOCHS} learning rate {LR}')
+    encoded_labels = {}
+    for i, label in enumerate(chosen_labels):
+        encoded_labels[label] = i
+
+    # Create dataset with transform
+    dataset = AudioDataset(dict_mats['A'], chosen_labels, encoded_labels, transform=transform)
+    datasetB = AudioDataset(dict_mats['B'], chosen_labels, encoded_labels, transform=transform)
+
+    # Create dataloaders
+    batch_size = 4
+    #train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    #val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(datasetB, batch_size=batch_size, shuffle=True)
+
+    ## Create an  instance of the model:
+    n_classes = len(chosen_labels)
+    model = AudioClassifNet(n_classes)
+
+    ## Run training:
+    print(f'Running training with {n_classes} classes')
+    out = run_training(model, train_loader, val_loader, n_classes, rate_l=LR, NUM_EPOCHS=NUM_EPOCHS, save=True)
+
+    ## Plot the confusion matrix:
+    sns.heatmap(out[2], annot=True, xticklabels=chosen_labels, yticklabels=chosen_labels)
