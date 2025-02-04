7083543ccec25cf2e9ff3a33345339d2317a235e
Roberto Duessmann <roberto.duessmann@gmail.com>
2025-02-04 22:47:50 +0100

    Clean up branch

--- app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-# Initialize FastAPI app
-app = FastAPI()
-
-@app.get("/test")  # Correct decorator for GET requests
-def hello():
-    return {"message": "hello world2"}  # Return a JSON response
